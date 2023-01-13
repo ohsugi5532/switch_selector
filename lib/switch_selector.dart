d27eee663a74039a26bb0e564a6eb345ad3f9887
ohsugi5532 <t.ohsugi5532@gmail.com>
2023-01-14 02:01:09 +0900

    feat: initial commit

---
+++ lib/switch_selector.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SwitchSelector extends StatefulWidget {
+  const SwitchSelector({
+    super.key,
+  });
+
+  @override
+  State<SwitchSelector> createState() => _SwitchSelectorState();
+}
+
+class _SwitchSelectorState extends State<SwitchSelector> {
+  @override
+  Widget build(BuildContext context) {
+    // UIの部分はここに書く。
+    return Container();
+  }
+}
